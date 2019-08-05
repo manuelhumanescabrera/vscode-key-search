b2526e7ea3fc44502421e150b654328d5f77f35e
Manuel Humanes Cabrera <manuel.humanes.cabrera@everis.com>
2019-08-05 16:10:29 +0200

    added input to declare keys

--- src/extension.ts
+++ src/extension.ts
@@ -4,18 +4,35 @@ import * as vscode from 'vscode';
 const loadJsonFile = require('load-json-file');
 var range: vscode.Selection; // rango seleccionado en el editor de texto
 var text: string; // texto seleccionado en el editor de texto
+let lastEntry: string = '';
 
 export function activate(context: vscode.ExtensionContext) {    
-    let inplace = vscode.commands.registerCommand('extension.filterTextInplace', (args?: {}) => filterText());
+    let inplace = vscode.commands.registerCommand('extension.filterTextInplace', (args?: {}) => filterText(args));
     
     context.subscriptions.push(inplace);
+}
+
+async function filterTextWrapper(args?: {}) {
+    if (typeof args === 'undefined') {
+        vscode.window.showInputBox({
+            placeHolder: 'Please enter a valid literal key',
+            value: lastEntry
+        }).then(async (entry: string) => {
+            console.log('entry', entry);
+            //filterText(entry);
+        });
+    } else if (('cmd' in args) && (typeof args['cmd'] === 'string')) {
+        console.log('args', args);
+    } else {
+        vscode.window.showErrorMessage('Invalid arguments passed. Must be a hash map with key \"cmd\" of type string.');
+    }
 }
 
 /**
  * Función que se ejecuta al usar el atajo de teclado. 
  */
-async function filterText() {
-
+async function filterText(args?: {}) {
+    
     // guardamos el directorio actual de trabajo
     // const cwd: string = getCurrentWorkingDirectory();
 
@@ -27,9 +44,47 @@ async function filterText() {
 
     // creamos la ruta completa donde buscará el archivo
     const pathFile = "C:\\Dev\\iberia\\workspaces\\web-iberia-plus-glp\\iberia-web-content\\literales\\iberia-plus\\es.json";
+    // const pathFile = "C:\\Dev\\Iberia\\workspace\\web-iberia-plus-glp\\iberia-web-content\\literales\\iberia-plus\\es.json";
 
     replaceString(pathFile, text);   
-    
+     
+}
+
+/**
+ * Función que sustituye la cadena seleccionada por la clave de su literal
+ * con el formato adecuado
+ * 
+ * @param path Ruta donde se encuentra el archivo de literales
+ * @param text Texto seleccionado en el editor
+ */
+async function replaceString(path: string, text: string) {
+    const fileObject = await loadJsonFile(path);
+    searchKey(text, fileObject);
+    //console.log('fileObject',fileObject);
+}
+
+/**
+ * Función que busca un texto dentro de un objeto de literales
+ * 
+ * @param text Texto seleccionado en el editor
+ * @param fileObject Objecto con las claves y los valores del archivo de literales
+ */
+function searchKey(text: string, fileObject) {
+    var keyExists = false;
+    // iteramos sobre los literales
+    for (let key in fileObject) {
+        // comparamos el texto seleccionado con los valores literales
+        if (text === fileObject[key]) {
+            keyExists = true;
+            // creamos la cadena con la que sustituimos el texto seleccionado
+            let newText = '{{\'' + key + '\'|translate}}';
+            setTextToSelectionRange(range, newText);
+        }
+    }
+
+    if(!keyExists) {
+        filterTextWrapper();
+    }
 }
 
 function getSelectionRange(): vscode.Selection {
@@ -68,9 +123,9 @@ function setTextToSelectionRange(range: vscode.Selection, text: string): void {
     let target = Promise.resolve(vscode.window.activeTextEditor);
     target.then((editor) => {
         editor.edit((editBuilder) => {
-            
-                editBuilder.replace(range, text);
-            
+
+            editBuilder.replace(range, text);
+
         });
         editor.revealRange(range);
     }, (reason: Error) => {
@@ -80,7 +135,7 @@ function setTextToSelectionRange(range: vscode.Selection, text: string): void {
 
 /**
  * Función que devuelve la ruta al directorio actual de trabajo
- * 
+ *
  * @returns string Directorio actual de trabajo
  */
 // function getCurrentWorkingDirectory(): string {
@@ -113,32 +168,32 @@ function setTextToSelectionRange(range: vscode.Selection, text: string): void {
 // }
 
 /**
- * Función que sustituye la cadena seleccionada por la clave de su literal
- * con el formato adecuado
- * 
- * @param path Ruta donde se encuentra el archivo de literales
- * @param text Texto seleccionado en el editor
+ * This implementation of the QuickPickItem contains the information necessary
+ * for displaying a quickpick item, as well as the command it should execute.
  */
-async function replaceString(path: string, text: string) {
-    const fileObject = await loadJsonFile(path);
-    searchKey(text, fileObject);
-    //console.log('fileObject',fileObject);
-}
+class Item implements vscode.QuickPickItem {
+    label: string;
+    description: string;
 
-/**
- * Función que busca un texto dentro de un objeto de literales
- * 
- * @param text Texto seleccionado en el editor
- * @param fileObject Objecto con las claves y los valores del archivo de literales
- */
-function searchKey(text: string, fileObject) {
-    // iteramos sobre los literales
-    for (let key in fileObject) {
-        // comparamos el texto seleccionado con los valores literales
-        if (text === fileObject[key]) {
-            // creamos la cadena con la que sustituimos el texto seleccionado
-            let newText = '{{' + key + '|translate}}';
-            setTextToSelectionRange(range, newText);
-        }
+    /**
+     * The literal command which the user wants to execute when this item is
+     * picked from the QuickPick list.
+     */
+    command: string;
+
+    constructor(label: string, description: string, command: string) {
+        this.label = label;
+        this.description = description;
+        this.command = command;
     }
-}
+
+    /**
+     * Runs the command.
+     */
+    run() {
+        let cmd = {
+            cmd: this.command
+        };
+        filterTextWrapper(cmd);
+    }
+};
